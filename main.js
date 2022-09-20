b56409d3367bd3930262352d964eb05c3ec9919d
SpookySnek <carleric.olsson@gmail.com>
2022-09-20 14:03:01 +0200

    Added stars to scene

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { ArrayCamera } from 'three';
 
 
 const scene = new THREE.Scene();
@@ -35,6 +36,19 @@ scene.add(lightHelper, gridHelper)
 //Remember to comment helper functions out when you are done with them
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
 
 function animate() {
     requestAnimationFrame( animate );
